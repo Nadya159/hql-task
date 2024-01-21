be7795390f8eb63baefad617551bfefa4749dc31
Nadya159 <138751846+Nadya159@users.noreply.github.com>
2024-01-21 12:28:54 +0500

    Add tests

--- hql-task/src/test/java/by/itacademy/hibernate/dao/UserDaoTest.java
+++ hql-task/src/test/java/by/itacademy/hibernate/dao/UserDaoTest.java
@@ -1,12 +1,17 @@
 package by.itacademy.hibernate.dao;
 
 
+import by.itacademy.hibernate.convertor.BirthdayConvertor;
+import by.itacademy.hibernate.dto.CompanyFilter;
+import by.itacademy.hibernate.entity.Birthday;
 import by.itacademy.hibernate.entity.Company;
 import by.itacademy.hibernate.utils.TestDataImporter;
 import by.itacademy.hibernate.entity.Payment;
 import by.itacademy.hibernate.entity.User;
 import by.itacademy.hibernate.util.HibernateUtil;
+import com.querydsl.core.Tuple;
 import lombok.Cleanup;
+import org.assertj.core.api.ListAssert;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.AfterAll;
@@ -14,6 +19,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -149,6 +155,81 @@ class UserDaoTest {
         List<Double> averagePayments = results.stream().map(r -> (Double) r[1]).collect(toList());
         assertThat(averagePayments).contains(500.0, 450.0);
 
+        session.getTransaction().commit();
+    }
+
+    @Test
+    void findMinAndMaxPaymentsOrderedByLastName(){
+        @Cleanup Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        List<Tuple> users = userDao.findMinAndMaxPaymentsOrderedByLastName(session);
+        assertThat(users).hasSize(5);
+
+        List<String> lastNames = users.stream().map(lastName -> lastName.get(0, String.class)).collect(toList());
+        assertThat(lastNames).contains("Brin", "Cook", "Gates", "Greene", "Jobs");
+
+        List<Integer> maxPayment = users.stream().map(maxPay -> maxPay.get(1, Integer.class)).collect(toList());
+        assertThat(maxPayment).contains(500);
+
+        List<Integer> minPayment = users.stream().map(minPay -> minPay.get(2, Integer.class)).collect(toList());
+        assertThat(minPayment).contains(100);
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    void findMaxName(){
+        @Cleanup Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        Integer maxName = userDao.findMaxName(session);
+        assertThat(maxName).isEqualTo(11);
+        session.getTransaction().commit();
+    }
+
+    @Test
+    void findBirthdayByCompany(){
+        @Cleanup Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        CompanyFilter filter = CompanyFilter.builder()
+                .name("Google")
+                .build();
+        List<Birthday> birthdays = userDao.findBirthdayByCompany(session, filter);
+        assertThat(birthdays).hasSize(2);
+
+        LocalDate convertor = birthdays.get(0).birthDate();
+        System.out.println("Convertor " + convertor);
+        assertThat(convertor).isEqualTo(LocalDate.of(1973, 8, 21));
+        session.getTransaction().commit();
+    }
+
+    @Test
+    void findCountUserInCompany(){
+        @Cleanup Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        List<Tuple> countUsers = userDao.findCountUserInCompany(session);
+        assertThat(countUsers).isNotEmpty();
+        assertThat(countUsers).hasSize(3);
+
+        List<Long> counts = countUsers.stream().map(it -> it.get(0, Long.class)).collect(toList());
+        assertThat(counts).contains(2L, 2L, 1L);
+        session.getTransaction().commit();
+    }
+
+    @Test
+    void findSumPaymentInCompany(){
+        @Cleanup Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        List<Tuple> sumPayments = userDao.findSumPaymentInCompany(session);
+        assertThat(sumPayments).isNotEmpty();
+        assertThat(sumPayments).hasSize(3);
+
+        List<Integer> sumPay = sumPayments.stream().map(it -> it.get(1, Integer.class)).collect(toList());
+        assertThat(sumPay).contains(2050, 2400, 900);
         session.getTransaction().commit();
     }
 }
